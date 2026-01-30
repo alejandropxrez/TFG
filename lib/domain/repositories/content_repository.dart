f0fbdbd33e1cd2fd6e744b328452d7eb435d594b
alejandropxrez <alexperezmonterroso@gmail.com>
2026-01-30 17:37:14 +0100

    chore: add docs for content_repository

--- lib/domain/repositories/content_repository.dart
+++ lib/domain/repositories/content_repository.dart
@@ -2,6 +2,9 @@ import 'package:algoquest/domain/entities/challenge_definition.dart';
 import 'package:algoquest/domain/entities/level_syllabus.dart';
 
 abstract class ContentRepository {
+  /// Fetches the syllabus for a specific level by its ID.
   Future<LevelSyllabus> getLevelSyllabus(String levelId);
+
+  /// Fetches the definition of a specific challenge by its ID.
   Future<ChallengeDefinition> getChallenge(String challengeId);
 }
