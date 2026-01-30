beb6a96f337a29f96951a5123ca21e477d864967
alejandropxrez <alexperezmonterroso@gmail.com>
2026-01-30 17:37:36 +0100

    chore: wire dependency on `AppComposition.build()`

--- lib/core/composition/app_composition.dart
+++ lib/core/composition/app_composition.dart
@@ -1,4 +1,8 @@
 import 'package:algoquest/core/constants/hive_type_ids.dart';
+import 'package:algoquest/data/datasources/local/asset_content_local_data_source.dart';
+import 'package:algoquest/data/datasources/local/content_local_data_source.dart';
+import 'package:algoquest/data/repositories/content_repository_impl.dart';
+import 'package:algoquest/domain/repositories/content_repository.dart';
 import 'package:hive_ce/hive.dart';
 
 import '../../data/datasources/local/hive_user_local_data_source.dart';
@@ -10,9 +14,13 @@ import 'hive_bootstrap.dart';
 
 class AppComposition {
   final UserRepository userRepository;
+  final ContentRepository contentRepository;
 
   // Avoid the constructor being called outside this class.
-  AppComposition._({required this.userRepository});
+  AppComposition._({
+    required this.userRepository,
+    required this.contentRepository,
+  });
 
   static Future<AppComposition> build() async {
     await HiveBootstrap.init();
@@ -26,6 +34,12 @@ class AppComposition {
     final userLocalDataSource = HiveUserLocalDataSource(userProgressBox);
     final userRepository = UserRepositoryImpl(userLocalDataSource);
 
-    return AppComposition._(userRepository: userRepository);
+    final contentLocalDataSource = AssetContentLocalDataSource();
+    final contentRepository = ContentRepositoryImpl(contentLocalDataSource);
+
+    return AppComposition._(
+      userRepository: userRepository,
+      contentRepository: contentRepository,
+    );
   }
 }
