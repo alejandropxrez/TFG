2c0c74d6520c82011ef6c48b79df03bcd96c6a18
alejandropxrez <alexperezmonterroso@gmail.com>
2026-03-24 19:06:56 +0100

    fix: update drift user local data source to upsert by userId

--- lib/data/datasources/local/drift_user_local_data_source.dart
+++ lib/data/datasources/local/drift_user_local_data_source.dart
@@ -1,5 +1,5 @@
-import 'package:algoquest/data/datasources/local/user_local_data_source.dart';
 import 'package:algoquest/data/datasources/local/drift/app_database.dart';
+import 'package:algoquest/data/datasources/local/user_local_data_source.dart';
 import 'package:algoquest/data/mappers/user_progress_drift_mapper.dart';
 import 'package:algoquest/data/models/user_progress_model.dart';
 
@@ -21,10 +21,19 @@ class DriftUserLocalDataSource implements UserLocalDataSource {
 
   @override
   Future<void> updateUserProgress(UserProgressModel userProgress) async {
+    final existing =
+        await (_database.select(_database.userProgressTable)
+              ..where((tbl) => tbl.userId.equals(userProgress.userId)))
+            .getSingleOrNull();
+
     final companion = UserProgressDriftMapper.toCompanion(userProgress);
 
-    await _database
-        .into(_database.userProgressTable)
-        .insertOnConflictUpdate(companion);
+    if (existing == null) {
+      await _database.into(_database.userProgressTable).insert(companion);
+    } else {
+      await (_database.update(_database.userProgressTable)
+            ..where((tbl) => tbl.userId.equals(userProgress.userId)))
+          .write(companion);
+    }
   }
 }
