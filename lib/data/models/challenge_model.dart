97ec13fb4ab798bb7184cea53cdb6392c3bac707
alejandropxrez <alexperezmonterroso@gmail.com>
2026-06-21 12:02:45 +0200

    fix: scope Freezed JsonKey analyzer exceptions to model files

--- lib/data/models/challenge_model.dart
+++ lib/data/models/challenge_model.dart
@@ -1,3 +1,6 @@
+// Freezed copies JsonKey annotations from factory parameters to generated fields.
+// ignore_for_file: invalid_annotation_target
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'package:algoquest/domain/entities/challenge_spec.dart';
--- lib/data/models/level_syllabus_model.dart
+++ lib/data/models/level_syllabus_model.dart
@@ -1,3 +1,6 @@
+// Freezed copies JsonKey annotations from factory parameters to generated fields.
+// ignore_for_file: invalid_annotation_target
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'level_syllabus_model.freezed.dart';
